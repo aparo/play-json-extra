710b56b561056c3c22f265b07f8c12bd544e183b
Alberto Paro <alberto.paro@gmail.com>
2015-09-16 10:53:44 +0200

    Removed inused code

--- play-json-extra/jvm/src/main/scala/play/json/extra/DateConstants.scala
+++ play-json-extra/jvm/src/main/scala/play/json/extra/DateConstants.scala
@@ -1,8 +1,6 @@
 package play.json.extra
 
-/**
-  * Created by alberto on 08/09/15.
-  */
 object DateConstants {
-   val millis= "yyyy-MM-dd'T'HH:mm:ss.SSS"
- }
+  val millis = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+}
+
